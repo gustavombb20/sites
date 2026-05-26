6572f55857ccbf3664ab2fc0a2715aab1ff72454
Gustavo <gustavombb20l@gmail.com>
2026-05-26 10:15:12 -0300

    Atualizando site

--- construtora/script.js
+++ construtora/script.js
@@ -37,3 +37,22 @@ const navLinks = document.querySelector(".nav-links");
 menuToggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+
+let lastScroll = 0;
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+
+    const currentScroll = window.pageYOffset;
+
+    if(currentScroll > lastScroll){
+        // descendo
+        header.style.transform = "translateY(-100%)";
+    } else {
+        // subindo
+        header.style.transform = "translateY(0)";
+    }
+
+    lastScroll = currentScroll;
+
+});
